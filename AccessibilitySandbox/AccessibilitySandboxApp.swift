33e4e9adc067e485c0dae9129ab182264455efe1
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-01 19:48:26 +0200

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Ihor Sukhachov on 01.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
