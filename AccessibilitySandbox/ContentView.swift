46b7d3bddecfc4ac928e83f3fecc5c4165b13fa1
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-02 15:31:19 +0200

    screen reader for increment and decrement buttons

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
-    
+//    let pictures = [
+//        "ales-krivec-15949",
+//        "galina-n-189483",
+//        "kevin-horstmann-141705",
+//        "nicolas-tissot-335096"
+//    ]
+//    
+//    let labels = [
+//        "Tulips",
+//        "Frozen tree buds",
+//        "Sunflowers",
+//        "Fireworks",
+//    ]
+//    
+//    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var value = 10
     var body: some View {
 //        Image(pictures[selectedPicture])
 //            .resizable()
@@ -45,14 +45,41 @@ struct ContentView: View {
 //        
 //        Image("character")
 //            .accessibilityHidden(true)
+//        VStack {
+//            Text("some text")
+//            
+//            Text("some other text")
+//                .font(Font.largeTitle.bold())
+//        }
+//        .accessibilityElement(children: .ignore)
+//        .accessibilityLabel("some text some other text")
+        
+        
         VStack {
-            Text("some text")
-            
-            Text("some other text")
-                .font(Font.largeTitle.bold())
+            Text("Value is: \(value)")
+            Button("Increment") {
+                value += 1
+            }
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel("some text some other text")
+        .accessibilityElement()
+        .accessibilityLabel("A simple counter")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+                
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+          
+        }
+        
     }
 }
 
